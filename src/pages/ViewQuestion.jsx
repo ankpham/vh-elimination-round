12bfacde23c7efc62fbb53a1549ccc16bd8126de
An Pham <anpham@cttdvnatl.org>
2022-07-16 08:52:52 -0400

    formatted question view

--- src/pages/ViewQuestion.jsx
+++ src/pages/ViewQuestion.jsx
@@ -10,9 +10,19 @@ const ViewQuestion = () => {
         <div className='view-question'> 
             <div className='container'>
                 <Link className="link back-to-selection" to="/">Back To Selection</Link> 
-                <h1 className='heading'>Question</h1>
-                <h5>Level {level}</h5>
-                <h5>Category {category}</h5>
+                <div className='question'>
+                    <div className='question-row'>
+                        <h5>Level {level}</h5>
+                        <h5>Category {category}</h5>
+                        <h1 className='question-heading'>This is a question</h1>
+                    </div>
+                    <ul className="choices">
+                        <li>Choice 1</li>
+                        <li>Choice 2</li>
+                        <li>Choice 3</li>
+                    </ul>
+                </div>
+
             </div>
         </div>
         </>
