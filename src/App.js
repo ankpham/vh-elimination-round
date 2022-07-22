7f2521b6f4d4c8c63e20af818dc21428b51682ba
An Pham <anpham@cttdvnatl.org>
2022-07-22 00:44:56 -0400

    new click sound effect

--- src/App.js
+++ src/App.js
@@ -11,6 +11,8 @@ import ViewQuestion from './pages/ViewQuestion';
 import AdminLogin from './pages/AdminLogin';
 import Admin from './pages/Admin';
 
+import audio from './assets/click-sound-effect.mp3';
+
 const App = () => {   
   const page = useRef(null);
   const fullScreenButton = useRef(null);
@@ -26,8 +28,15 @@ const App = () => {
     }
   }
 
+  let clickSoundEffect = new Audio(audio);
+
+  const playSound = () => {
+    clickSoundEffect.play();
+    clickSoundEffect = new Audio(audio)
+  }
+
   return (
-    <div ref={page} className="app">
+    <div ref={page} onClickCapture={() => playSound()} className="app">
     <Router>
       <Routes>
         <Route path="/" element={<Home/>} exact/>
